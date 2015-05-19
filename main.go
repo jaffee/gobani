0f8ffbfa936f05601786d3559ce967cde392fa60
Matt Jaffee <jaffee@umbel.com>
2015-05-18 23:39:50 -0500

    full working RPS with move validation and scoring

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"math/rand"
 	"net"
+	"strings"
 )
 
 type player struct {
 	Name string
 	conn net.Conn
 }
+
+var legalMoves = []string{"rock", "paper", "scissors"}
 
 func (p *player) SendMsg(s string) (n int, err error) {
 	bs := []byte(s)
@@ -80,6 +83,10 @@ func qwatcher(q chan player) {
 
 func theThunderdome(p1 player, p2 player) {
 	welcome(p1, p2)
+	p1score := 0
+	p2score := 0
+	var p1msg string
+	var p2msg string
 	for {
 		ch1 := make(chan string, 1)
 		go getMove(p1, ch1)
@@ -87,15 +94,59 @@ func theThunderdome(p1 player, p2 player) {
 		go getMove(p2, ch2)
 		move1 := <-ch1
 		move2 := <-ch2
+		winner := getWinner(move1, move2)
+		if winner == 1 {
+			p1score++
+			p1msg = "win"
+			p2msg = "lose"
+		} else if winner == 2 {
+			p2score++
+			p1msg = "lose"
+			p2msg = "win"
+		} else {
+			p1msg = "tie"
+			p2msg = "tie"
+		}
+
 		fmt.Printf("Player 1 played %v, and p2 played %v\n", move1, move2)
-		p1.SendMsg(fmt.Sprintf("%v played %v and you played %v\n", p2.Name, move2, move1))
-		p2.SendMsg(fmt.Sprintf("%v played %v and you played %v\n", p1.Name, move1, move2))
+		p1.SendMsg(fmt.Sprintf("%v:%v played %v and you:%v played %v. You %v.\n", p2.Name, p2score, move2, p1score, move1, p1msg))
+		p2.SendMsg(fmt.Sprintf("%v:%v played %v and you:%v played %v. You %v.\n", p1.Name, p1score, move1, p2score, move2, p2msg))
 	}
 }
 
+func getWinner(m1 string, m2 string) int {
+	m1index := stringIndex(m1, legalMoves)
+	m2index := stringIndex(m2, legalMoves)
+	if m1index == m2index {
+		return 0
+	} else if (m1index+1)%len(legalMoves) == m2index {
+		return 2
+	} else {
+		return 1
+	}
+
+}
+
+func stringIndex(s string, sslice []string) int {
+	for i, str := range sslice {
+		if s == str {
+			return i
+		}
+	}
+	return -1
+}
+
 func getMove(p player, ch chan string) {
-	ans := p.RecvMsg()
-	ch <- ans
+	for {
+		ans := p.RecvMsg()
+		for i := 0; i < len(legalMoves); i++ {
+			if ans == legalMoves[i] {
+				ch <- ans
+				return
+			}
+		}
+		p.SendMsg(fmt.Sprintf("That's not a legal move! Try one of these: %v\n", strings.Join(legalMoves, ", ")))
+	}
 }
 
 func welcome(p1 player, p2 player) {
