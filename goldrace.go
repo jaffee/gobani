eed022ad455891152f0dddf8ee2bb0188158d7ee
Matt Jaffee <jaffee@umbel.com>
2015-05-27 23:57:08 -0500

    it aint too pretty... but Player can move up now!

--- goldrace.go
+++ goldrace.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/jaffee/gobani/game"
+	"math/rand"
 	"strings"
 )
 
@@ -16,12 +17,23 @@ var width = 10
 func goldrace(ps ...game.Player) {
 	battlefield := makeBattlefield(height, width)
 	ps[0].SendMsg(battlefield.toString())
+	for {
+		msg := ps[0].RecvMsg()
+		if msg == "u" {
+			battlefield.Move("P", "u")
+		}
+		ps[0].SendMsg(battlefield.toString())
+	}
 }
 
 type battlefield struct {
 	height int
 	width  int
 	rep    [][]string
+	px     int
+	py     int
+	gx     int
+	gy     int
 }
 
 func makeBattlefield(height int, width int) battlefield {
@@ -32,8 +44,15 @@ func makeBattlefield(height int, width int) battlefield {
 			rep[i][j] = "O"
 		}
 	}
+	px := rand.Intn(width)
+	py := rand.Intn(height)
+	gx := rand.Intn(width)
+	gy := rand.Intn(height)
 
-	return battlefield{height, width, rep}
+	rep[py][px] = "P"
+	rep[gy][gx] = "G"
+
+	return battlefield{height, width, rep, px, py, gx, gy}
 }
 
 func (b *battlefield) toString() string {
@@ -42,4 +61,12 @@ func (b *battlefield) toString() string {
 		lines[i] = strings.Join(row, "")
 	}
 	return strings.Join(lines, "\n") + "\n"
+}
+
+func (b *battlefield) Move(thing string, move string) {
+	if move == "u" && b.py-1 >= 0 {
+		b.rep[b.py][b.px] = "O"
+		b.py = b.py - 1
+		b.rep[b.py][b.px] = "P"
+	}
 }
