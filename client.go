fc82886b61342c248409e8fc28b10306a4d73dbf
Matt Jaffee <jaffee@umbel.com>
2015-06-17 22:46:30 -0500

    move stdin reading to goroutine and make term raw

--- client.go
+++ client.go
@@ -3,45 +3,54 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"golang.org/x/crypto/ssh/terminal"
 	"net"
 	"os"
 	"time"
 )
 
 func main() {
+	oldState, err := terminal.MakeRaw(0)
+	if err != nil {
+		panic(err)
+	}
+	defer terminal.Restore(0, oldState)
+
 	conn, err := net.Dial("tcp", "localhost:80")
 	if err != nil {
 		panic(err)
 	}
 
+	reader := bufio.NewReader(os.Stdin)
+	stdinMsgs := make(chan byte)
+	go readBuff(reader, stdinMsgs)
+
 	msgs := make(chan string, 200)
 	var read_string string
-	reader := bufio.NewReader(os.Stdin)
 	go readConn(conn, msgs)
+
 	for {
 		select {
 		case read_string = <-msgs:
-			fmt.Println("read")
+			// fmt.Println("read")
 		default:
 			read_string = ""
 		}
 		if read_string != "" {
-			fmt.Println("Received:")
+			// fmt.Println("Received:")
 			fmt.Println(read_string)
 		}
 
-		if reader.Buffered() > 0 {
-			fmt.Println("something buffered")
-			b, err := reader.ReadByte()
+		select {
+		case read_byte := <-stdinMsgs:
+			_, err := conn.Write([]byte{read_byte})
 			if err != nil {
 				panic(err)
 			}
-			conn.Write([]byte{b})
-		} else {
-			fmt.Println("nothing buffered")
+			// fmt.Println("wrote to conn", n)
+		default:
+			time.Sleep(time.Millisecond * 10)
 		}
-		time.Sleep(time.Second * 1)
-
 	}
 
 }
@@ -50,12 +59,29 @@ func readConn(conn net.Conn, msgs chan string) {
 	recBuff := make([]byte, 1000)
 	for {
 		n, err := conn.Read(recBuff)
-		fmt.Println("hey")
 		if err != nil {
 			panic(err)
 		}
+		// fmt.Println("Read bytes from conn ", n)
 		msgs <- string(recBuff[:n])
 		zeroBuff(recBuff)
+	}
+}
+
+func readBuff(reader *bufio.Reader, msgs chan byte) {
+	for {
+		b, err := reader.ReadByte()
+		// fmt.Println("Read byte ", b)
+		if err != nil {
+			panic(err)
+		}
+		msgs <- b
+		select {
+		case <-msgs:
+			break
+		default:
+			continue
+		}
 	}
 }
 
